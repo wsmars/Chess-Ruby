06a4ca4fe186f936efce7056ce2e7dc5483e2828
App Academy Student <student@appacademy.io>
2016-01-11 15:08:58 -0800

    add display and cursor module

--- board.rb
+++ board.rb
@@ -1,8 +1,10 @@
+require 'colorize'
+
 class Board
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(8) { Array.new(8) }
+    @grid = Array.new(8) { Array.new(8, Piece.new(' ')) }
     populate_grid
   end
 
@@ -13,37 +15,47 @@ class Board
         # color = :white if [6,7].include?(row)
 
         #pawn
-        self[row, col] = Piece.new('P') if [1,6].include?(row)
+        self[[row, col]] = Piece.new('P') if [1,6].include?(row)
 
         #rook
-        self[row, col] = Piece.new('R') if [[0,0],[0,7],[7,0],[7,7]].include?([row,col])
+        self[[row, col]] = Piece.new('R') if [[0,0],[0,7],[7,0],[7,7]].include?([row,col])
 
         #knight
-        self[row, col] = Piece.new('N') if [[0,1],[0,6],[7,1],[7,6]].include?([row,col])
+        self[[row, col]] = Piece.new('N') if [[0,1],[0,6],[7,1],[7,6]].include?([row,col])
 
         #bishop
-        self[row, col] = Piece.new('B') if [[0,2],[0,5],[7,2],[7,5]].include?([row,col])
+        self[[row, col]] = Piece.new('B') if [[0,2],[0,5],[7,2],[7,5]].include?([row,col])
 
         #queen
-        self[row, col] = Piece.new('Q') if [[0,3],[7,3]].include?([row,col])
+        self[[row, col]] = Piece.new('Q') if [[0,3],[7,3]].include?([row,col])
 
         #king
-        self[row, col] = Piece.new('K') if [[0,4],[7,4]].include?([row,col])
+        self[[row, col]] = Piece.new('K') if [[0,4],[7,4]].include?([row,col])
       end
     end
   end
 
-  def [](row, col)
+  def move(start, end_pos)
+    #re-write after implementing Display
+    self[end_pos] = self[start]
+    self[start] = nil
+  end
+
+  def [](pos)
+    row = pos[0]
+    col = pos[1]
     @grid[row][col]
   end
 
-  def []=(row, col, value)
+  def []=(pos, value)
+    row = pos[0]
+    col = pos[1]
     @grid[row][col] = value
   end
 
-  def inspect
-    @grid
-  end
+  # def inspect
+  #   @grid
+  # end
 end
 
 class Piece
@@ -51,7 +63,11 @@ class Piece
     @name = name
   end
 
-  def inspect
-    @name
+  def to_s
+    @name.to_s
   end
+
+  # def inspect
+  #   @name.colorize(:red)
+  # end
 end
---
+++ display.rb
@@ -0,0 +1,48 @@
+require "colorize"
+require_relative "board"
+require_relative "cursorable"
+
+class Display
+  include Cursorable
+
+  def initialize(board)
+    @board = board
+    @cursor_pos = [0, 0]
+  end
+
+  def build_grid
+    @board.grid.map.with_index do |row, i|
+      build_row(row, i)
+    end
+  end
+
+  def build_row(row, i)
+    row.map.with_index do |piece, j|
+      color_options = colors_for(i, j)
+      piece.to_s.colorize(color_options)
+    end
+  end
+
+  def colors_for(i, j)
+    if [i, j] == @cursor_pos
+      bg = :light_red
+    elsif (i + j).odd?
+      bg = :light_blue
+    else
+      bg = :green
+    end
+    { background: bg, color: :white }
+  end
+
+  def render
+    system("clear")
+    build_grid.each { |row| puts row.join }
+    nil
+  end
+end
+
+if __FILE__ != $PROGRAM_NAME
+  b = Board.new
+  d = Display.new(b)
+  d.render
+end
